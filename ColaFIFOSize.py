c9778ce854872ad5dc4da4d03f640be2d4ef83de
mauriciopintos <mauricioenrique.pintos@estudiantes.unahur.edu.ar>
2021-04-30 21:02:51 -0300

    Objeto Cola con tamaño

--- ColaFIFOSize.py
+++ ColaFIFOSize.py
@@ -1,15 +1,27 @@
-
+import threading
 class ColaFIFOsize:
 
     def __init__(self,size):
         self.elementos = []
         self.size = size
+        self.condition = threading.Condition()
+
     def insertar(self, dato):
+        self.condition.acquire()
+        if len(self.elementos) == self.size:
+            self.condition.wait()
         self.elementos.append(dato)
-        return dato
+        self.condition.notify(1)
+        self.condition.release()
 
     def extraer(self):
-        return self.elementos.pop(0)
+        self.condition.acquire()
+        while len(self.elementos) == 0:
+            self.condition.wait()
+        elemento = self.elementos.pop(0)
+        self.condition.notify(1)
+        self.condition.release()
+        return elemento
 
     def ultimo(self):
         return self.elementos[-1]
@@ -26,7 +38,7 @@ class ColaFIFOsize:
 
 
 def main():
-    cola = ColaFIFO()
+    cola = ColaFIFOsize(9)
 
     # check if esta_vacia()
 
---
+++ programaEjercicioSize.py
@@ -0,0 +1,61 @@
+import threading
+import time
+import random
+import logging
+from ColaFIFOSize import ColaFIFOsize
+
+from ColaFIFOSize import ColaFIFOsize
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+class Productor(threading.Thread):
+    def __init__(self,cola,retardo):
+        super().__init__()
+        self.cola = cola
+        self.retardo = retardo
+
+    def run(self):
+        while True:
+            self.cola.insertar(random.randint(0,100))
+            logging.info(f'esta produciendo {self.cola.ultimo()}')
+            time.sleep(self.retardo)
+
+        # cola.insertar(6)
+        # pass // pasa la funcion sin ejecutar
+
+class Consumidor(threading.Thread):
+    def __init__(self,cola,retardo):
+        super().__init__()
+        self.cola = cola
+        self.retardo = retardo
+
+    def run(self):
+        while True:
+            elemento = self.cola.extraer()
+            logging.info(f'consumio el elemento {elemento}')
+            time.sleep(self.retardo)
+
+
+def main():
+    hilos = []
+    cola = ColaFIFOsize(8)
+
+    for i in range(3):
+        productor = Productor(cola,random.randint(1,5))
+        consumidor = Consumidor(cola,random.randint(1,5))
+
+        hilos.append(productor)
+        hilos.append(consumidor)
+
+        logging.info(f'Arrancando productor {productor.name}')
+        productor.start()
+
+        logging.info(f'Arrancando consumidor {consumidor.name}')
+        consumidor.start()
+
+    for thr in hilos:
+        thr.join()
+
+
+if __name__ == '__main__':
+    main()
